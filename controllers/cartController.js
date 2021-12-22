cceac654c6fa5b5a596218838119314acd02c58a
area268457913 <area268457913@hotmail.com>
2021-12-23 01:17:51 +0800

    完成把商品加入購物車

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,6 +4,7 @@ const CartItem = db.CartItem
 const Product = db.Product
 const cartController = {
     getCart: (req, res) => {
+        console.log(req.session)
         Cart.findAndCountAll({
             raw: true,
             nest: true,
@@ -12,44 +13,124 @@ const cartController = {
             ]
         })
             .then(carts => {
-                console.log(carts.rows)
+                // console.log(carts.rows)
                 return res.render('carts', {
                     carts: carts.rows
                 })
             }).catch(error => {
                 console.log(error)
             })
-
     },
     postCart: (req, res) => {
+
         return Cart.findOrCreate({
             where: {
                 id: req.session.cartId || 0,
-            }
-        }).spread(function (cart, created) {
-            return CartItem.findOrCreate({
-                where: {
-                    CartId: cart.id,
-                    ProductId: req.body.ProductId
+            },
+        }).then((cart) => {
+            let [carts, create] = [cart[0], cart[1]]
+            console.log('===============================')
+            console.log(create)
+            console.log('===============================')
 
-                },
-                default: {
-                    CartId: cart.id,
-                    ProductId: req.body.ProductId
-                }
-            }).spread(function (cartItem, created) {
-                return cartItem.update({
-                    quantity: (cartItem.quantity || 0)
-                        + 1,
-                }).then((cartItem) => {
-                    req.session.cartId = cart.id
-                    return req.session.save(() => {
-                        return res.redirect('back')
-                    })
+            if (create) {
+                console.log("進入create")
+                console.log("CartId:")
+                console.log(carts.dataValues.id)
+                console.log("ProductId")
+                console.log(req.body.productId)
+                console.log("session")
+                console.log(req.session)
+
+                CartItem.findAndCountAll({
+                    where: {
+                        CartId: carts.dataValues.id,
+                        ProductId: req.body.productId
+                    }
+                }).then(cartItems => {
+                    console.log("進入cartItems")
+
+                    if (cartItems.count === 0) {
+                        console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+
+                        // console.log(cartItems)
+                        CartItem.create({
+                            CartId: carts.dataValues.id,
+                            ProductId: req.body.productId,
+                            quantity: 1,
+
+                        }).then(newcart => {
+
+                        }).catch(error => {
+                            console.log(error)
+                        })
+                    } else {
+
+                        console.log('33333333333333333333333333333333333333333333')
+
+                        // console.log(cartItems)
+
+
+                    }
+                    // console.log(cartItems)
                 })
-            })
-        })
-    }
+                req.session.cartId = carts.dataValues.id
+                return req.session.save(() => {
+                    return res.redirect('back')
+                })
+            } else {
+
+                console.log("進入else")
+                console.log("CartId:")
+                console.log(carts.dataValues.id)
+                console.log("ProductId")
+                console.log(req.body.productId)
+                console.log("session")
+                console.log(req.session)
+
+                CartItem.findOne({
+                    where: {
+                        CartId: carts.dataValues.id,
+                        ProductId: req.body.productId
+                    }
+                }).then(cartupdate => {
+                    console.log('====================================')
+
+                    console.log(cartupdate)
+                    if (cartupdate) {
+                        cartupdate.update({
+
+                            quantity: cartupdate.dataValues.quantity + 1,
+
+                        }).then(newcart => {
+                            req.session.cartId = carts.dataValues.id
+                            return req.session.save(() => {
+                                return res.redirect('back')
+                            })
+                        }).catch(error => {
+                            console.log(error)
+                        })
+                    } else {
+                        CartItem.create({
+                            CartId: carts.dataValues.id,
+                            ProductId: req.body.productId,
+                            quantity: 1,
+
+                        }).then(newcart => {
+                            req.session.cartId = carts.dataValues.id
+                            return req.session.save(() => {
+                                return res.redirect('back')
+                            })
+                        }).catch(error => {
+                            console.log(error)
+                        })
+                    }
+
+                })
+            }
+
+        });
+    },
 }
 
 module.exports = cartController
