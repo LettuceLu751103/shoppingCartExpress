1fa7f7daace3a1fcdd5d9826e610b26b55b0a647
area268457913 <area268457913@hotmail.com>
2021-12-29 01:16:52 +0800

    完成成立訂單功能

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,18 +2,17 @@ const db = require('../models')
 const OrderItem = db.OrderItem
 const Order = db.Order
 const Product = db.Product
+const Cart = db.Cart
 
 const orderController = {
     getOrders: (req, res) => {
         console.log('=== 進入 getOrder ===')
         Order.findAll({
             include: 'items',
+            order: [['createdAt', 'DESC']]
 
         }).then(orders => {
-            console.log(orders)
-            console.log(orders[0].dataValues)
-            console.log('==================================')
-            console.log(orders[0].dataValues.items)
+
 
             return res.render('orders', { orders })
         })
@@ -23,6 +22,29 @@ const orderController = {
     postOrders: (req, res) => {
         console.log('=== 進入 postOrder ===')
         console.log(req.body)
+        return Cart.findByPk(req.body.cartId, { include: 'items' }).then(cart => {
+            return Order.create(
+                { name, address, phone, shipping_status, payment_status, amount } = req.body
+            ).then(order => {
+
+                let results = [];
+                for (let i = 0; i < cart.items.length; i++) {
+                    console.log(order.id, cart.items[i].id)
+                    results.push(
+                        OrderItem.create({
+                            OrderId: order.id,
+                            ProductId: cart.items[i].id,
+                            price: cart.items[i].price,
+                            quantity: cart.items[i].CartItem.quantity,
+                        })
+                    );
+                }
+
+                return Promise.all(results).then(() =>
+                    res.redirect('/orders')
+                );
+            })
+        })
     }
 }
 
